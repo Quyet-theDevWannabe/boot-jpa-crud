9e32543dc2819764d2ef782f3e21d8a4ed679eb4
Quyet <laiquyetDEV@gmail.com>
2021-04-21 16:45:43 +0700

    Initialized using http://start.spring.io/: web, devTools, mySQL, JPA

---
+++ src/test/java/personal/guides/springboot/bootjpacrud/BootJpaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.guides.springboot.bootjpacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJpaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
